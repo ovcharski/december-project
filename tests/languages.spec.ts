961f0e4774e5fba94814df720c18321c380313c8
ovcharski <48056945+ovcharski@users.noreply.github.com>
2024-12-27 12:15:09 +0200

    language test

--- tests/languages.spec.ts
+++ tests/languages.spec.ts
@@ -4,46 +4,6 @@ import { OrganizePage } from '../pages/OrganizePage';
 
 let organizePage: OrganizePage;
 
-test('Change language OG', async ({ page }) => {
-  await expect(page.getByTestId('open')).toContainText('Choose File');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Argentina').click();
-  await expect(page.getByTestId('open')).toContainText('Elegir archivo');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Brasil').click();
-  await expect(page.getByTestId('open')).toContainText('Escolher arquivo');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Canada (Français)').click();
-  await expect(page.getByTestId('open')).toContainText('Choisissez un fichier');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('ישראל').click();
-  await expect(page.getByTestId('open')).toContainText('בחירת קובץ');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Nederland', { exact: true }).click();
-  await expect(page.getByTestId('open')).toContainText('Bestand kiezen');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Deutschland').click();
-  await expect(page.getByTestId('open')).toContainText('Datei auswählen');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('Italia').click();
-  await expect(page.getByTestId('open')).toContainText('Scegli file');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('България').click();
-  await expect(page.getByTestId('open')).toContainText('Изберете файл');
-  await page.getByTestId('languageToggle').click();
-
-  await page.getByText('日本語').click();
-  await expect(page.getByTestId('open')).toContainText('ファイルを選択');
-});
-
 test('Change language Refactored', async ({ page }) => {
 
   const languages = [
